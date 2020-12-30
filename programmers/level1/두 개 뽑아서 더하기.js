ff65bb82b8214f15c53da369635e4b64ba394240
Mingyu3 <aeneascon@naver.com>
2020-12-30 22:06:20 +0900

    두 개 뽑아서 더하기

---
+++ programmers/level1/두 개 뽑아서 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+  let myset = new Set();
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      //Set으로 중복 제거
+      myset.add(numbers[i] + numbers[j]);
+    }
+  }
+  let answer = [...myset];
+  return answer.sort((a, b) => a - b);
+}
