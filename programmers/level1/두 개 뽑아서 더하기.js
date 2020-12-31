a300e4fa65e2e512a5822d161a40458060e2c4ae
aeneascon <aeneascon@gmail.com>
2020-12-31 11:00:38 +0900

    크레인 인형뽑기 게임

---
+++ programmers/level1/두 개 뽑아서 더하기.js
@@ -0,0 +1,9 @@
+//https://programmers.co.kr/learn/courses/30/lessons/68644
+function solution(numbers) {
+  var answer = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {}
+  }
+
+  return answer;
+}
