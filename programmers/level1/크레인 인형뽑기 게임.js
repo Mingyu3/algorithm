4a5d5e07cb81640b69a237e82a17a87a0d96fede
Mingyu3 <aeneascon@naver.com>
2021-01-03 01:11:07 +0900

    크레인 인형뽑기 게임

--- programmers/level1/크레인 인형뽑기 게임.js
+++ programmers/level1/크레인 인형뽑기 게임.js
@@ -1,25 +1,30 @@
+//https://programmers.co.kr/learn/courses/30/lessons/64061
 function solution(board, moves) {
   let answer = 0;
   const basket = [];
-    for (let move of moves) {
-      let moveIndex = move - 1;
-      for (let j = 0; j < board.length; j++) {
-        if (board[j][moveIndex]) {
-          basket.push(board[i][moveIndex]);
-          board[j][moveIndex] = 0;
-          break;
-        }
-        for (let doll in basket) {
-          if (basket[doll] === basket[doll + 1]) {
-            basket.splice(doll, 1);
-            answer++;
+  for (let move of moves) {
+    let moveIndex = move - 1;
+    for (let i = 0; i < board.length; i++) {
+      //공백아닐 때
+      if (board[i][moveIndex]) {
+        basket.push(board[i][moveIndex]);
+        board[i][moveIndex] = 0;
+        for (let i = 0; i < basket.length; i++) {
+          ////바스켓에 같은 인형 2개 쌓였을 때
+          if (basket[i] === basket[i + 1]) {
+            basket.splice(i, 2);
+            answer += 2;
           }
         }
+        break;
       }
     }
+  }
+
   return answer;
 }
 
+//0은 빈칸, 숫자에 따라 다른 인형
 const board = [
   [0, 0, 0, 0, 0],
   [0, 0, 1, 0, 3],
@@ -27,5 +32,8 @@ const board = [
   [4, 2, 4, 4, 2],
   [3, 5, 1, 3, 1],
 ];
+
+//크레인이 인형 뽑는 위치(인덱스+1)
 const moves = [1, 5, 3, 5, 1, 2, 1, 4];
 solution(board, moves);
+//뽑힌 인형: 4, 3, 1, 1, 3, 2, 4
