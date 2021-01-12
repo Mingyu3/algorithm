9c9ea1768e5cd0456ad24a190b0a0040514400f0
Mingyu3 <aeneascon@naver.com>
2021-01-12 23:49:23 +0900

    체육복 진행중

--- programmers/level1/체육복.js
+++ programmers/level1/체육복.js
@@ -1,32 +1,33 @@
 function solution(n, lost, reserve) {
-  var answer = n - lost.lenght;
   for (let i = 0; i < reserve.length; i++) {
     for (let j = 0; j < lost.length; j++) {
       if (reserve[i] === lost[j]) {
         reserve.splice(i, 1);
         lost.splice(j, 1);
+        break;
       }
     }
   }
-
+  let answer = n - lost.length;
   for (let i = 0; i < lost.length; i++) {
     for (let j = 0; j < reserve.length; j++) {
-      if (lost[i] === reserve[j] + 1 || lost[i] === reserve[j] - 1) {
+      if (lost[i] - 1 === reserve[j] || lost[i] + 1 === reserve[j]) {
         lost.splice(i, 1);
         reserve.splice(j, 1);
         answer++;
+        i--;
+        j--;
+        break;
       }
     }
   }
-  console.log(answer);
-
   return answer;
 }
 
 //전체학생수
-const n = 5;
+const n = 10;
 //도난당한 학생번호
-const lost = [2, 4];
+const lost = [1, 2, 4, 6, 8, 10];
 //여벌의 체육복 가져온 학생번호
-const reserve = [1, 3, 5];
+const reserve = [1, 3, 5, 7, 9, 10];
 solution(n, lost, reserve);
